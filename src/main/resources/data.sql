26834158836a48aaf52077eeae63dd9e8592a200
KevinSanchez5 <@KevinSanchez5>
2024-11-20 19:43:09 +0100

    Fix data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(id, name, product_type, description, interest, created_at, updated_at, is_deleted)
+VALUES ('1695ce31-d70a-48d9-af8b-90dea3d1e664','CUENTA CORRIENTE', 'CUENTA BANCARIA', 'descripcion cuenta corriente', 0.0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+INSERT INTO products(id, name, product_type, description, interest, created_at, updated_at, is_deleted)
+VALUES ('5f9e55c2-4053-4351-8666-5c3692efdac9','CUENTA AHORRO', 'CUENTA BANCARIA', 'descripcion cuenta bancaria', 1.5, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
