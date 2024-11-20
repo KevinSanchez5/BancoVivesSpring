163da41d3ad0bf36506e3e54819eb6ed7b66f7f7
TomasVaquerin <vaquerinjimenez@gmail.com>
2024-11-20 15:32:52 +0100

    add updateProductFromInput

--- src/main/java/vives/bancovives/rest/products/mappers/ProductMapper.java
+++ src/main/java/vives/bancovives/rest/products/mappers/ProductMapper.java
@@ -4,12 +4,13 @@ import vives.bancovives.rest.products.dto.input.InputProduct;
 import vives.bancovives.rest.products.dto.output.OutputProduct;
 import vives.bancovives.rest.products.model.Product;
 
+import java.time.LocalDateTime;
+
 /**
  * Esta clase proporciona métodos para mapear entre objetos {@link Product} y {@link OutputProduct}, así como entre
  * objetos {@link InputProduct} y {@link Product}.
  */
 public class ProductMapper {
-    private ProductMapper() {}
 
     /**
      * Mapea un objeto {@link Product} a un objeto {@link OutputProduct}.
@@ -43,5 +44,19 @@ public class ProductMapper {
                 .productType(inputProduct.getProductType().trim().toUpperCase())
                 .interest(inputProduct.getInterest())
                 .build();
+    }
+
+    /**
+     * Actualiza los campos de un objeto {@link Product} existente con los valores de un objeto {@link InputProduct}.
+     *
+     * @param existingProduct el objeto {@link Product} que se va a actualizar
+     * @param updatedProduct  el objeto {@link InputProduct} que contiene los nuevos valores
+     */
+    public static void updateProductFromInput(Product existingProduct, InputProduct updatedProduct) {
+        existingProduct.setName(updatedProduct.getName().trim().toUpperCase());
+        existingProduct.setProductType(updatedProduct.getProductType().trim().toUpperCase());
+        existingProduct.setInterest(updatedProduct.getInterest());
+        existingProduct.setDescription(updatedProduct.getDescription());
+        existingProduct.setUpdatedAt(LocalDateTime.now());
     }
 }
