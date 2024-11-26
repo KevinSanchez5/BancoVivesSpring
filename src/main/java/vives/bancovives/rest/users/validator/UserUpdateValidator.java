2dd37264de11efc5d9943533da8ac092fadb9b5e
KevinSanchez5 <@KevinSanchez5>
2024-11-26 18:04:22 +0100

    Refactorizado el validador clientes

--- src/main/java/vives/bancovives/rest/users/validator/UserUpdateValidator.java
+++ src/main/java/vives/bancovives/rest/users/validator/UserUpdateValidator.java
@@ -3,19 +3,35 @@ package vives.bancovives.rest.users.validator;
 import org.springframework.stereotype.Component;
 import vives.bancovives.rest.users.dto.input.UserUpdateDto;
 import vives.bancovives.rest.users.exceptions.UserBadRequest;
+import vives.bancovives.rest.users.models.Role;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class UserUpdateValidator {
 
     public void validateUpdate(UserUpdateDto dto) {
-        if (dto.getUsername() == null  && dto.getPassword() == null && dto.getRoles() == null) {
-            throw new UserBadRequest("Username y password no pueden estar vacíos");
+        validateEmptyAttributes(dto.getUsername(), dto.getPassword(), dto.getRoles());
+        validatePassword(dto.getPassword());
+        validateUsername(dto.getUsername());
+    }
+
+    private void validateEmptyAttributes(String username, String password, Set<Role> roles){
+        if(username==null && password==null && roles.isEmpty() || roles == null){
+            throw new UserBadRequest("Los campos no pueden estar vacíos");
         }
-        if(dto.getPassword()!= null && dto.getPassword().length() < 5 && dto.getPassword().isBlank()){
+    }
+
+    private void validatePassword(String password){
+        if(password!=null && password.length() < 5 && password.isBlank()){
             throw new UserBadRequest("La contraseña debe tener al menos 5 caracteres");
         }
-        if(dto.getUsername()!= null && dto.getUsername().trim().isBlank()){
-            throw new UserBadRequest("El nombre de usuario no puede estar en blanco");
+    }
+
+    private void validateUsername(String username){
+        if(username!=null && username.trim().isBlank()){
+            throw new UserBadRequest("El nombre de usuario debe tener al menos 5 caracteres");
         }
     }
 }
